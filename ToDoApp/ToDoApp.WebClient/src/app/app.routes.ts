8d696e6c4e8e21b34199259b02ab19286919b932
dmytro.sleptsov <sleptsov.dima@gmail.com>
2024-12-06 15:25:49 +0200

    TDA-11 changed TaskAddComponent as modal window

--- ToDoApp/ToDoApp.WebClient/src/app/app.routes.ts
+++ ToDoApp/ToDoApp.WebClient/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { TaskListComponent } from './components/task/task-list/task-list.component';
-import { TaskAddComponent } from './components/task/task-add/task-add.component';
 import { TaskFormComponent } from './components/task/task-form/task-form.component';
 import { RegistrationComponent } from './components/auth/registration/registration.component';
 import { LoginComponent } from './components/auth/login/login.component';
@@ -9,7 +8,6 @@ import { authGuard } from './guards/auth.guard';
 export const routes: Routes = [
     { path: '', redirectTo: '/tasks', pathMatch: 'full' },
     { path: 'tasks', component: TaskListComponent, canActivate: [authGuard] },
-    { path: 'tasks/add', component: TaskAddComponent, canActivate: [authGuard] },
     { path: 'tasks/:id', component: TaskFormComponent, canActivate: [authGuard] },
     { path: 'registration', component: RegistrationComponent },
     { path: 'login', component: LoginComponent },
--- ToDoApp/ToDoApp.WebClient/src/app/components/task/task-add/task-add.component.ts
+++ ToDoApp/ToDoApp.WebClient/src/app/components/task/task-add/task-add.component.ts
@@ -1,33 +1,45 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component, ElementRef, EventEmitter,
+  OnDestroy, Output, ViewChild
+} from '@angular/core';
 import { TaskService } from '../../../services/task.service';
-import { Router } from '@angular/router';
-
 import { Task } from '../../../models/task.model';
-import { ReactiveFormsModule, FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subject, takeUntil } from 'rxjs';
 import { CommonModule } from '@angular/common';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-task-add',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, CommonModule],
+  imports: [ ReactiveFormsModule, CommonModule],
   templateUrl: './task-add.component.html',
   styleUrl: './task-add.component.scss'
 })
-export class TaskAddComponent implements OnDestroy {
+export class TaskAddComponent implements OnDestroy, AfterViewInit {
   taskForm: FormGroup;
   private destroy$ = new Subject<void>();
+  @ViewChild('addTaskModal') addTaskModal!: ElementRef;
+  @Output() dataSubmitted = new EventEmitter<Task>();
+
+  private modalInstance: any;
 
   constructor(
     private formBuilder: FormBuilder,
-    private taskService: TaskService,
-    private router: Router,
+    private taskService: TaskService
   ) {
-    this.taskForm = formBuilder.group({
-      name: ['', Validators.required],
-      description: [''],
-      isCompleted: [false]
-    });
+    this.taskForm = this.createTaskForm();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  ngAfterViewInit(): void {
+    this.initializeModal();
   }
 
   onSubmit(): void {
@@ -35,14 +47,42 @@ export class TaskAddComponent implements OnDestroy {
       let task: Task = this.taskForm.value;
       this.taskService.createTask(task)
         .pipe(takeUntil(this.destroy$))
-        .subscribe(() =>
-          this.router.navigate([''])
-        );
+        .subscribe((task) => {
+          this.dataSubmitted.emit(task);
+          this.modalInstance.hide();
+          this.resetForm();
+        });
     }
   }
 
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
+  showModal(): void {
+    this.resetForm();
+    this.modalInstance?.show();
+  }
+
+  resetForm(): void {
+    this.taskForm.reset({
+      name: '',
+      description: '',
+      isCompleted: false,
+    });
+
+    this.taskForm.markAsPristine();
+    this.taskForm.markAsUntouched();
+    this.taskForm.updateValueAndValidity();
+  }
+
+  private createTaskForm(): FormGroup {
+    return this.formBuilder.group({
+      name: ['', Validators.required],
+      description: [''],
+      isCompleted: [false]
+    });
+  }
+
+  private initializeModal(): void {
+    if (this.addTaskModal?.nativeElement) {
+      this.modalInstance = new bootstrap.Modal(this.addTaskModal.nativeElement);
+    }
   }
 }
